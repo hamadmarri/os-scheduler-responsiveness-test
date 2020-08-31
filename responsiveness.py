9e1974c9ea11043b3ba92dee58342ed8049518ea
Hamad Marri <hamad.s.almarri@gmail.com>
2020-09-01 00:47:57 +0300

    add more details, + benchmark cachy vs cfs

--- responsiveness.py
+++ responsiveness.py
@@ -9,21 +9,29 @@ from random import *
 
 
 
-def interactive(results):
+def interactive(results, iterations):
 	while True:
 		r = randint(1, 3)
 		if r == 1:
 			start = timer()
 			time.sleep(r)
+			print("sorting")
 			array = [randint(1, 10000) for j in range(0, 10000)]
 			array2 = sorted(array)
 			end = timer()
 			results.value = results.value + (end - start) - r
 		elif r == 2:
+			start = timer()
+			time.sleep(r)
 			f = open("almarri", "r")
 			print(f.read())
 			f.close()
+			end = timer()
+			results.value = results.value + (end - start) - r
 		else:
+			start = timer()
+			time.sleep(r)
+			print("reading/writing to almarri2")
 			if os.path.exists("almarri2"):
 				os.remove("almarri2")
 				
@@ -34,6 +42,10 @@ def interactive(results):
 			
 			f1.close()
 			f2.close()
+			end = timer()
+			results.value = results.value + (end - start) - r
+		
+		iterations.value = iterations.value + 1
 			
 			
 			
@@ -58,6 +70,7 @@ def main():
 	interactive_threads = []
 	prime_time = []	
 	interactive_time = []
+	interactive_iterations = []
 	
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-i", help="number of interactive threads", type=int)
@@ -81,11 +94,14 @@ def main():
 	
 	if args.i:
 		interactive_time = [0 for i in range(0, args.i)]
+		interactive_iterations = [0 for i in range(0, args.i)]
 		
 		for i in range(0, args.i):
 			interactive_time[i] = multiprocessing.Value("d", 0.0, lock=False)
+			interactive_iterations[i] = multiprocessing.Value("i", 0, lock=False)
 			interactive_threads.append( \
-			multiprocessing.Process(target=interactive, args=(interactive_time[i],)))
+				multiprocessing.Process(target=interactive, \
+				args=(interactive_time[i], interactive_iterations[i],)))
 		
 		for i in range(0, args.i):
 			interactive_threads[i].start()
@@ -100,14 +116,16 @@ def main():
 		for i in range(0, args.i):
 			interactive_threads[i].terminate()
 	
-	print("\n\n")
+	print("\n")
 	if args.p:	
 		for i in range(0, args.p):
-			print("prime time: ", prime_time[i].value)
+			print(i, "prime time: ", prime_time[i].value)
 	
 	if args.i:	
 		for i in range(0, args.i):
-			print("interactive time: ", interactive_time[i].value)
+			print(i, "total response time: ", interactive_time[i].value, \
+			" runs: ", interactive_iterations[i].value, \
+			" average: ", interactive_time[i].value / interactive_iterations[i].value)
 	
 	# if args.f:
 	
